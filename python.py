cf950d909cc5f977d0b2725c19cbb9b16024b787
RonaldoDairo <programaciondairo@gmail.com>
2023-07-01 19:25:26 +0200

    feat: insert excel

---
+++ python.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+
+df_data_history = pd.read_csv('fifa_worldcup_matches.csv')
+df_fixture = pd.read_csv('fifa_worldcup_fixture.csv')
+missing_data= pd.read_csv('fifa_worldcup_missing_data.csv')
